5a9b6509b0b553e65c25b5518ba3ab610f951987
Jules95Game <romp.julian1@gmail.com>
2024-07-29 15:46:12 +0200

    Added Backend

---
+++ backend/src/test/java/com/brajula/blinddate/BlindDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brajula.blinddate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
