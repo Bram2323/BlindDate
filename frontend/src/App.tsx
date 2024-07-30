1b5004e54c47ac63c7bb97bc679038631eb2f3ce
Bram2323 <bram10111213@gmail.com>
2024-07-30 10:52:09 +0200

    Added api service

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
+import { history } from "./services/History";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function App() {
+    history.navigate = useNavigate();
+    history.location = useLocation();
+
     return (
         <>
             <div></div>
