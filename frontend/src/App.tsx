c8694ee6c164fb42414ee649e2053dda7242aaba
Bram2323 <bram10111213@gmail.com>
2024-07-30 10:43:14 +0200

    Fixed tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <div className=" bg-red-800">test</div>
+        </>
+    );
 }
 
 export default App;
