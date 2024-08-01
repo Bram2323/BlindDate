8d6ba511a5b8739fcf288a9ba1a4fe33727ebcac
Laila <lvanrooijen@live.nl>
2024-08-01 15:09:01 +0200

    added login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { history } from "./services/History";
-import { useNavigate, useLocation, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home/Home";
+import {history} from "./services/History";
+import {useNavigate, useLocation, Routes, Route} from "react-router-dom";
+import {Home} from "./pages/Home/Home";
 import NavBar from "./components/NavBar";
 import Register from "./pages/Register/Register";
-
+import Login from "./pages/Login/Login";
 function App() {
     history.navigate = useNavigate();
     history.location = useLocation();
@@ -15,6 +15,7 @@ function App() {
             <div className="w-full h-full overflow-y-auto">
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                 </Routes>
             </div>
