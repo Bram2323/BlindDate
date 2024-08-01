ed23b833ee2080eee992bf0fd2f29a760c1cd023
Jules95Game <romp.julian1@gmail.com>
2024-08-01 14:44:37 +0200

    Added test profile

--- frontend/src/components/Profile/ProfileBox.tsx
+++ frontend/src/components/Profile/ProfileBox.tsx
@@ -1,4 +1,17 @@
+import Genders from "./Genders";
+import Profile from "./Profile";
+import Sexualities from "./Sexualities";
+
 function ProfileBox() {
+    let profile: Profile = {
+        id: 1,
+        userName: "SuperJohn",
+        description: "I'm a cool person",
+        dateOfBirth: Date.parse("05-05-1995"),
+        gender: Genders.male,
+        sexuality: Sexualities.hetroSexual,
+    };
+
     return (
         <>
             <div></div>
