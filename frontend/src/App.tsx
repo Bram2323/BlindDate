515796305f77a70642d4cb69eb52286db743917b
Jules95Game <romp.julian1@gmail.com>
2024-08-01 10:56:10 +0200

    Added NavBar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,8 @@
 import "./App.css";
-import {history} from "./services/History";
-import {useNavigate, useLocation, Routes, Route} from "react-router-dom";
-import {Home} from "./pages/Home";
+import { history } from "./services/History";
+import { useNavigate, useLocation, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
     history.navigate = useNavigate();
@@ -9,6 +10,7 @@ function App() {
 
     return (
         <>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
