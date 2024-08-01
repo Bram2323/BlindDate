57a12f0448dab1a999fcc0053df1f99111da2e7b
Laila <lvanrooijen@live.nl>
2024-08-01 15:32:16 +0200

    re adding stuff

---
+++ backend/src/main/java/com/brajula/blinddate/entities/profile/Gender.java
@@ -0,0 +1,7 @@
+package com.brajula.blinddate.entities.profile;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    NONBINARY
+}
---
+++ backend/src/main/java/com/brajula/blinddate/entities/profile/ProfileRepository.java
@@ -0,0 +1,5 @@
+package com.brajula.blinddate.entities.profile;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileRepository extends JpaRepository<Profile, Long> {}
