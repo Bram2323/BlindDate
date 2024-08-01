f8aa1d6013e24395a381773692ebf0beff336d92
Jules95Game <romp.julian1@gmail.com>
2024-08-01 11:38:23 +0200

    Added NavBar content

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,5 +1,13 @@
 function NavBar() {
-    return <>NavBar</>;
+    return (
+        <>
+            <div>
+                <button>Blind Date</button>
+                <button>Inloggen</button>
+                <button>Registreren</button>
+            </div>
+        </>
+    );
 }
 
 export default NavBar;
