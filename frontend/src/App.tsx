629d2f39d76330f9c5939793519ced775208d639
Laila <lvanrooijen@live.nl>
2024-08-03 19:12:47 +0200

    uncomment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
                         </>
                     )}
 
-                    {/* <Route path="*" element={<Navigate to="/" />} /> */}
+                    <Route path="*" element={<Navigate to="/" />} />
                 </Routes>
             </div>
         </>
