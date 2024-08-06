f8cb0c15949e5ff2b97f70c8edd93b0fe60dd27b
Jules95Game <romp.julian1@gmail.com>
2024-08-06 12:25:47 +0200

    Added Judging page content

--- frontend/src/pages/Judging/Judging.tsx
+++ frontend/src/pages/Judging/Judging.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import ApiService from "../../services/ApiService";
+import Profile from "../../components/Profile/Profile";
+import ProfileBox from "../../components/Profile/ProfileBox";
+
 function Judging() {
+    const [profiles, setProfiles] = useState<Profile[]>();
+    const [firstPfpSrc, setFirstPfpSrc] = useState<string>("");
+
+    useEffect(() => {
+        ApiService.get("profiles")
+            .then((response) => {
+                setProfiles(response.data);
+                ApiService.get(
+                    `images/${response.data[0].imageId}`,
+                    null,
+                    "blob"
+                ).then((response) => {
+                    if (response.data instanceof Blob) {
+                        const url = URL.createObjectURL(response.data);
+                        setFirstPfpSrc(url);
+                    }
+                });
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
     return (
         <>
-            <div></div>
+            <div className="flex h-full justify-evenly">
+                <div className="flex items-center">
+                    <button className="text-8xl bg-red-300 hover:bg-red-500 p-8 rounded-full">
+                        ❌
+                    </button>
+                </div>
+                {profiles && (
+                    <ProfileBox profile={profiles[0]} pfpSrc={firstPfpSrc} />
+                )}
+                <div className="flex items-center">
+                    <button className="text-8xl text-green-600 bg-green-300 hover:text-green-800 hover:bg-green-500 p-8 rounded-full">
+                        ✔
+                    </button>
+                </div>
+            </div>
         </>
     );
 }
