1f7f79c6d1a4eb102692e2b104829090dbfaed83
Jules95Game <romp.julian1@gmail.com>
2024-08-08 14:30:12 +0200

    Solve merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,9 @@
 import "./App.css";
 import history from "./services/History";
-import {
-    useNavigate,
-    useLocation,
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import { useNavigate, useLocation, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import NavBar from "./components/NavBar";
 import Register from "./pages/Register/Register";
-import { useUser } from "./services/UserService";
 import Login from "./pages/Login/Login";
 import { CreateProfile } from "./pages/Profile/CreateProfile";
 import { ProfileView } from "./pages/Profile/ProfileView";
@@ -20,29 +13,20 @@ function App() {
     history.navigate = useNavigate();
     history.location = useLocation();
 
-    const [, isLoggedIn] = useUser();
-
     return (
         <>
             <NavBar />
             <div className="w-full h-full overflow-y-auto">
                 <Routes>
-                    {isLoggedIn ? (
-                        <>
-                            <Route path="/" element={<Judging />} />
-                            <Route
-                                path="/create-profile"
-                                element={<CreateProfile />}
-                            />
-                            <Route path="/profile" element={<ProfileView />} />
-                        </>
+                    {sessionStorage.getItem("JWT") ? (
+                        <Route path="/" element={<Judging />} />
                     ) : (
-                        <>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/login" element={<Login />} />
-                            <Route path="/register" element={<Register />} />
-                        </>
+                        <Route path="/" element={<Home />} />
                     )}
+                    <Route path="/create-profile" element={<CreateProfile />} />
+                    <Route path="/profile" element={<ProfileView />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
                 </Routes>
             </div>
         </>
