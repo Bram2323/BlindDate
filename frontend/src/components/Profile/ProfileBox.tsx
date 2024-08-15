e6ea7524a8813dcec4318e2e4afc7d165c8d5ab4
Jules95Game <romp.julian1@gmail.com>
2024-08-15 11:57:43 +0200

    Remove PFP from judge page

--- frontend/src/components/Profile/ProfileBox.tsx
+++ frontend/src/components/Profile/ProfileBox.tsx
@@ -1,6 +1,6 @@
 import Profile from "./Profile";
 
-function ProfileBox({ profile, pfpSrc }: { profile: Profile; pfpSrc: string }) {
+function ProfileBox({ profile }: { profile: Profile }) {
     let profileAge: number = Math.floor(
         Math.abs(Date.now() - new Date(profile.dateOfBirth).getTime()) /
             (1000 * 3600 * 24) /
@@ -10,10 +10,6 @@ function ProfileBox({ profile, pfpSrc }: { profile: Profile; pfpSrc: string }) {
     return (
         <>
             <div className="bg-blue-300 flex flex-col items-center my-8 px-16 py-8 rounded-lg shadow-xl">
-                <img
-                    src={`${pfpSrc}`}
-                    alt={`Profile picture of ${profile.username}`}
-                />
                 <p>{profile.username}</p>
                 <p>{profileAge}</p>
                 <p>{profile.description}</p>
--- frontend/src/pages/Judging/Judging.tsx
+++ frontend/src/pages/Judging/Judging.tsx
@@ -5,22 +5,11 @@ import ProfileBox from "../../components/Profile/ProfileBox";
 
 function Judging() {
     const [profiles, setProfiles] = useState<Profile[]>();
-    const [firstPfpSrc, setFirstPfpSrc] = useState<string>("");
 
     useEffect(() => {
         ApiService.get("profiles")
             .then((response) => {
                 setProfiles(response.data);
-                ApiService.get(
-                    `images/${response.data[0].imageId}`,
-                    null,
-                    "blob"
-                ).then((response) => {
-                    if (response.data instanceof Blob) {
-                        const url = URL.createObjectURL(response.data);
-                        setFirstPfpSrc(url);
-                    }
-                });
             })
             .catch((error) => {
                 console.error(error);
@@ -35,9 +24,7 @@ function Judging() {
                         ❌
                     </button>
                 </div>
-                {profiles && (
-                    <ProfileBox profile={profiles[0]} pfpSrc={firstPfpSrc} />
-                )}
+                {profiles && <ProfileBox profile={profiles[0]} />}
                 <div className="flex items-center">
                     <button className="text-8xl text-green-600 bg-green-300 hover:text-green-800 hover:bg-green-500 p-8 rounded-full">
                         ✔
