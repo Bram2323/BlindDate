6ebc4015aab4b4f45f80579152a115b883e69f1d
Jules95Game <romp.julian1@gmail.com>
2024-08-15 16:25:52 +0200

    Added JudgementRepository

---
+++ backend/src/main/java/com/brajula/blinddate/entities/judgment/JudgementRepository.java
@@ -0,0 +1,5 @@
+package com.brajula.blinddate.entities.judgment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JudgementRepository extends JpaRepository<Judgement, Long> {}
