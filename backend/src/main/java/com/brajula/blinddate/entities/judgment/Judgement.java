38f439c4600e4d379d4cdc6860aca4954af178b6
Jules95Game <romp.julian1@gmail.com>
2024-08-15 16:57:51 +0200

    Added postJudgement

--- backend/src/main/java/com/brajula/blinddate/entities/judgment/Judgement.java
+++ backend/src/main/java/com/brajula/blinddate/entities/judgment/Judgement.java
@@ -5,12 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class Judgement {
     @Id @GeneratedValue private Long id;
 
--- backend/src/main/java/com/brajula/blinddate/entities/judgment/JudgementController.java
+++ backend/src/main/java/com/brajula/blinddate/entities/judgment/JudgementController.java
@@ -4,9 +4,11 @@ import com.brajula.blinddate.Routes;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(Routes.JUDGEMENTS)
 public class JudgementController {
     private final JudgementRepository judgementRepository;
+
+    @PostMapping
+    public ResponseEntity<Judgement> postJudgement(
+            @RequestBody Judgement judgement, UriComponentsBuilder ucb) {
+        Judgement savedJudgement = judgementRepository.save(judgement);
+        URI location = ucb.path("{id}").buildAndExpand(savedJudgement.getId()).toUri();
+        return ResponseEntity.created(location).body(savedJudgement);
+    }
 }
