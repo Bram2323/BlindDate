23bd71f71fff57f79323da9798060e3f7012de98
Laila <lvanrooijen@live.nl>
2024-08-17 17:42:18 +0200

    styling

--- frontend/src/pages/Profile/components/Section.tsx
+++ frontend/src/pages/Profile/components/Section.tsx
@@ -5,9 +5,10 @@ export const Section: React.FC<SectionProps> = ({
     label,
     style: style,
 }) => {
+    const breakpoints = "sm:w-2/3 lg:w-2/4";
     return (
         <div
-            className={`${style} my-4 px-4 py-8 rounded-lg flex flex-col items-center justify-center w-3/4 shadow-lg border-2 border-gray-800`}
+            className={`${style} ${breakpoints} w-full my-4 px-4 py-8 rounded-lg flex flex-col items-center justify-center shadow-lg border-2 border-gray-800`}
         >
             {children}
         </div>
